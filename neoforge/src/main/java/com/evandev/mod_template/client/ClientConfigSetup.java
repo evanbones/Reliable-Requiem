35053d56b4f202295f6f682007712d18fe8bf89f
Evan Bowness <bownessevan@gmail.com>
2026-04-21 17:44:33 -0700

    Initialize 26.1

---
+++ neoforge/src/main/java/com/evandev/mod_template/client/ClientConfigSetup.java
@@ -0,0 +1,14 @@
+package com.evandev.mod_template.client;
+
+import com.evandev.mod_template.client.integration.ClothConfigIntegration;
+import com.evandev.mod_template.platform.Services;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+public class ClientConfigSetup {
+    public static void register(ModContainer container) {
+        if (Services.PLATFORM.isModLoaded("cloth_config")) {
+            container.registerExtensionPoint(IConfigScreenFactory.class, (c, parent) -> ClothConfigIntegration.createScreen(parent));
+        }
+    }
+}
